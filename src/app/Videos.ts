6269b207bd4f4002af0dbbb519a67a6eccc2e1a9
Yuri Belousov <thetoksin@gmail.com>
2023-12-16 23:07:51 +0300

    Update Videos.ts

--- src/app/Videos.ts
+++ src/app/Videos.ts
@@ -15,7 +15,7 @@ export class Videos {
     /*14*/"/v0/b/advent-calendar-4a616.appspot.com/o/sticker_14.webm?alt=media&token=82fffc04-748e-4b3e-a019-3a526e5df01f",
     /*15*/"/v0/b/advent-calendar-4a616.appspot.com/o/sticker_15.webm?alt=media&token=c4192fb9-6354-4d3c-bdc0-fb6672f43f9d",
     /*16*/"/v0/b/advent-calendar-4a616.appspot.com/o/sticker_16.webm?alt=media&token=5e1fa409-8984-40a8-af99-2dbac1a07587",
-    /*17*/"",
+    /*17*/"/v0/b/advent-calendar-4a616.appspot.com/o/sticker_17.webm?alt=media&token=fe2e2b66-6392-49b6-866a-a3a51ef7822e",
     /*18*/"/v0/b/advent-calendar-4a616.appspot.com/o/sticker_18.webm?alt=media&token=dec94ec3-2f26-4b08-a968-51242ffaaf1b",
     /*19*/"",
     /*20*/"/v0/b/advent-calendar-4a616.appspot.com/o/sticker_20.webm?alt=media&token=f5556dcc-592f-4a7f-8ea5-6d20813ef9d4",
